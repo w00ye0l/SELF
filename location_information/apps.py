f0ad0017670dc6fb7ee81371c7241b116a73fad2
psun0610 <psy010610@naver.com>
2022-10-19 01:36:41 +0900

    app 등록

---
+++ location_information/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LocationInformationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'location_information'
--- pjt/settings.py
+++ pjt/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'location_information',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'asia/seoul'
 
 USE_I18N = True
 
